e3ca6c04106ccfd15eba26b3423f94709aaeb522
mudar.noufal <mudar.noufal@740add52-eb35-9737-33fe-8ef7962b6fce>
2010-12-15 04:10:01 +0000

    Display search header (query)
    git-svn-id: https://patiner-montreal-for-android.googlecode.com/svn/trunk@36 740add52-eb35-9737-33fe-8ef7962b6fce

--- src/ca/mudar/patinoires/PatinoiresSearchable.java
+++ src/ca/mudar/patinoires/PatinoiresSearchable.java
@@ -5,12 +5,10 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
-
 
 
 /**
@@ -19,109 +17,107 @@ import android.widget.AdapterView.OnItemClickListener;
  */
 public class PatinoiresSearchable extends ListActivity {
 	private static final String TAG = "PatinoiresSearchable";
-	
-    private static final int MENU_SEARCH = 1;
 
-//    private TextView mTextView;
-//    private ListView mList;
-    private PatinoiresOpenData mDbHelper;
-    private Cursor cursor;
-    
-    //TODO
-    String interfaceLanguage;
-  
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        mDbHelper = PatinerMontreal.getmDbHelper();
-        interfaceLanguage = "en";
-                
-        
-        Intent intent = getIntent();
+	private static final int MENU_SEARCH = 1;
 
-        setContentView(R.layout.rinks_list);
+	private PatinoiresOpenData mDbHelper;
+	private Cursor cursor;
+
+	//TODO
+	String interfaceLanguage;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mDbHelper = PatinerMontreal.getmDbHelper();
+		interfaceLanguage = "en";
 
 
-        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
-Log.w( TAG , "rink found = " + intent.getDataString());
+		Intent intent = getIntent();
 
-//long rinkId = intent.getData();
+		setContentView(R.layout.rinks_list);
+
+
+		if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+//			Log.w( TAG , "rink found = " + intent.getDataString());
+
+			//long rinkId = intent.getData();
 			long rinkId = 1;
 			displayDetails( rinkId );
 
-            finish();
- 
-        } 
-        else if ( Intent.ACTION_SEARCH.equals( intent.getAction() ) ) {
-            handleIntent(getIntent());
+			finish();
 
-        	/*
-        	String query = intent.getStringExtra( SearchManager.QUERY );
-
-        	fillData( query );
-        	*/
-        }
+		} 
+		else if ( Intent.ACTION_SEARCH.equals( intent.getAction() ) ) {
+			handleIntent(getIntent());
+		}
 
 		getListView().setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 				displayDetails( arg3 );
 			} } );
-        
+		/*        
         Log.d( TAG , intent.toString());
         if (intent.getExtras() != null) {
             Log.d( TAG , intent.getExtras().keySet().toString());
         }
-    }
-    
-    @Override
-    protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        handleIntent(intent);
-    }
+		 */
+	}
 
-    private void handleIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-          String query = intent.getStringExtra(SearchManager.QUERY);
-          fillData( query );
-        }
-    }
-    
-    private void displayDetails( Long rinkId ) {
+	@Override
+	protected void onNewIntent(Intent intent) {
+		setIntent(intent);
+		handleIntent(intent);
+	}
+
+	private void handleIntent(Intent intent) {
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			fillData( query );
+		}
+	}
+
+	private void displayDetails( Long rinkId ) {
 		Intent intent = new Intent( getApplicationContext() , PatinoiresDetails.class );
 		intent.putExtra( "rinkId" , rinkId );
 		intent.putExtra( "interfaceLanguage" , interfaceLanguage );
 		startActivity( intent);
-    }
-    
-    private void fillData( String query ) {
-    	mDbHelper.openDb();
-    	
-    	cursor = mDbHelper.searchRinks( query , null );
-    	startManagingCursor( cursor );
+	}
 
-    	String[] from = new String[] { 
-    			PatinoiresDbAdapter.KEY_RINKS_NAME, 
-    			( interfaceLanguage.equals( "fr" ) ? PatinoiresDbAdapter.KEY_RINKS_DESC_FR : PatinoiresDbAdapter.KEY_RINKS_DESC_EN ) ,
-    			PatinoiresDbAdapter.KEY_BOROUGHS_NAME ,
-    			PatinoiresDbAdapter.KEY_BOROUGHS_REMARKS 
+	private void fillData( String query ) {
 
-    	};
-    	int[] to = new int[] { R.id.l_rink_name , R.id.l_rink_desc , R.id.l_borough_name , R.id.l_borough_remarks };
+		TextView mTextView = ( TextView ) findViewById( R.id.search_query );
+		mTextView.setVisibility( View.VISIBLE );
+		mTextView.setText( getString( R.string.search_results, query ) );
 
-    	RinksListCursorAdapter rinks = new RinksListCursorAdapter(this, R.layout.rinks_list_item , cursor, from, to, mDbHelper.isSortOnBorough() );
-    	setListAdapter( rinks );
+		mDbHelper.openDb();
 
-    	mDbHelper.closeDb();
-    }
+		cursor = mDbHelper.searchRinks( query , null );
+		startManagingCursor( cursor );
 
-    protected void onResume() {
-    	super.onResume();
-    	
-    	Intent intent = getIntent();
-    	String query = intent.getStringExtra( SearchManager.QUERY );
-Log.w( TAG , "onResume query = " + query );
-    	fillData(query);
-    }
-    
+		String[] from = new String[] { 
+				PatinoiresDbAdapter.KEY_RINKS_NAME, 
+				( interfaceLanguage.equals( "fr" ) ? PatinoiresDbAdapter.KEY_RINKS_DESC_FR : PatinoiresDbAdapter.KEY_RINKS_DESC_EN ) ,
+				PatinoiresDbAdapter.KEY_BOROUGHS_NAME ,
+				PatinoiresDbAdapter.KEY_BOROUGHS_REMARKS 
+
+		};
+		int[] to = new int[] { R.id.l_rink_name , R.id.l_rink_desc , R.id.l_borough_name , R.id.l_borough_remarks };
+
+		RinksListCursorAdapter rinks = new RinksListCursorAdapter(this, R.layout.rinks_list_item , cursor, from, to, mDbHelper.isSortOnBorough() );
+		setListAdapter( rinks );
+
+		mDbHelper.closeDb();
+	}
+
+	protected void onResume() {
+		super.onResume();
+
+		Intent intent = getIntent();
+		String query = intent.getStringExtra( SearchManager.QUERY );
+
+		fillData(query);
+	}
+
 }
